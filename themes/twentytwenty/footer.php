4697607bd92c66b888c6615d3c77908028de4676
Koo Jie Yao <44890238+JY08042@users.noreply.github.com>
2020-01-15 13:59:26 +0800

    My initial commit message

--- themes/twentytwenty/footer.php
+++ themes/twentytwenty/footer.php
@@ -31,7 +31,7 @@
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
                 
-								<?php _e( 'Powered by Team 10!', 'twentytwenty' ); ?>
+								<?php _e( 'Powered by CMS Team 10!', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-Team10 -->
 
